3eaf5b01b3173de83f8a05f20b580b23a1209414
Denis Karandeev <den.strongman@gmail.com>
2017-01-21 11:49:46 +0800

    sample web server

---
+++ RESM.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/plain")
+	w.Write([]byte("Hello Wdfdf!!!"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":80", nil)
+}
