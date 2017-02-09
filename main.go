c425812efec5f5369d6f3fcc7cd5afdb69b4edd4
Denis Karandeev <den.strongman@gmail.com>
2017-02-09 21:05:30 +0800

    added http.HandleFunc for list, reset

--- main.go
+++ main.go
@@ -5,28 +5,32 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Resources Manager - RESM"))
-	resources(r)
-}
+var st []int
+var (
+	resour = []string{"r1", "r2", "r3"}
+)
 
 func main() {
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/allocate/", allocate)
+	http.HandleFunc("/deallocate/", deallocate)
+	http.HandleFunc("/list/", list)
+	http.HandleFunc("/reset/", reset)
+	http.ListenAndServe(":9000", nil)
 
 }
 
+/*
 func resources(r *http.Request) {
 	var (
-		clientResor     = [][]string{}      //двумерный slice для сохранения данных клиент-ресурс
-		resourDealloc   = []string{}        //одномерный slice для сохранения данных по deallocated
-		chResourDealloc = make(chan string) // канал для данных
+		clientResor     = [][]string{}               //двумерный slice для сохранения данных клиент-ресурс
+		resourDealloc   = []string{"r1", "r2", "r3"} //одномерный slice для сохранения данных по deallocated
+		chResourDealloc = make(chan string)          // канал для данных
 	)
 	path := r.URL.Path() // додумать как работать с путем
 	//path := r.URL.Path(2)
 	switch path {
 	case "allocate":
-		allocate(resourDealloc)
+		allocate(resourDealloc, clientResor)
 	case "deallocate":
 		deallocate(clientResor, resourDealloc)
 	case "list":
@@ -37,39 +41,59 @@ func resources(r *http.Request) {
 		BadRequest()
 	}
 }
-
-func allocate(resourDealloc []string) {
+*/
+func allocate(w http.ResponseWriter, r *http.Request) {
 	//если localhost:80/allocate/alice то есть Path(2: по /), то берется 1 ресурс клиенту
-	if len(resourDealloc) != 0 {
-		//прописать логику
-	} else {
-		fmt.Println("Out of resources")
-	}
+	/*	if len(resourDealloc) != 0 {
+			res := resourDealloc[0]
+			resourDealloc[0] = ""
+			clientResor = res
+			//прописать логику
+		} else {
+			fmt.Println("Out of resources")
+		}*/
+	//s := append(st, 3)
+	w.Write([]byte("Resources Manager - RESM\n"))
+	path := r.URL.Path[1:]
+	fmt.Fprintf(w, "Команда: %s!\n", path)
+	fmt.Fprintf(w, string(resour[0]))
+	s := append(resour, "2")
+	fmt.Fprintf(w, string(s[3]))
+	resour[0] = ""
+	fmt.Fprintf(w, string(resour[0]))
+
 }
 
-func deallocate(clientResor [][]string, resourDealloc []string) {
-	for range clientResor {
+func deallocate(d http.ResponseWriter, r *http.Request) {
+	/*for range clientResor {
 		//прописать логику
 	}
-	clientResor = [][]string{}
+	clientResor = [][]string{}*/
 	//localhost:8080/deallocate/r1 то в канал добавляется r1
+	w.Write([]byte("Resources Manager - RESM\n"))
+	path := r.URL.Path[1:]
+	fmt.Fprintf(w, "Команда: %s!\n", path)
 
 }
 
-func list(clientResor [][]string, resourDealloc []string) {
+//func list(clientResor [][]string, resourDealloc []string) // может пригодится
+func list(l http.ResponseWriter, r *http.Request) {
 	// "allocated":{"r1":"alice"}, "deallocated":["r2","r3"]
-	fmt.Printf("allocaetd: {%s}, deallocated: %d\n", clientResor, resourDealloc)
+	//	fmt.Fprintf("allocaetd: {%s}, deallocated: %d\n", clientResor, resourDealloc)*/
+	fmt.Fprintf(l, "list")
 }
 
-func reset(clientResor [][]string, chResourDealloc chan string) {
+func reset(res http.ResponseWriter, r *http.Request) {
 	//Все ресурсы из слайса в канал передать и удалить всех клиентов
-	for resour := range clientResor {
+	/*	for resour := range clientResor {
 		chResourDealloc <- clientResor
 		clientResor := ""
-	}
+	}*/
+	fmt.Fprintf(res, "reset")
 }
 
+/*
 func BadRequest() {
 	//Ошибка
 	fmt.Println("Bad Request")
-}
+}*/
